02ae2a2e1b9bcfd43f58cb9657873749e8ccdf1a
Danilo Santos <danilo.carreiro@etec.sp.gov.br>
2019-12-28 14:41:33 -0300

    updated description package

--- lib/flutter_response_status.dart
+++ lib/flutter_response_status.dart
@@ -15,52 +15,47 @@ class ResponseStatus<T1, T2> {
   T2 data;
   dynamic dataError;
 
-  ResponseStatus(this.status, this.message, {this.code, this.data, this.dataError});
+  ResponseStatus(this.status, this.message,
+      {this.code, this.data, this.dataError});
 
-  ResponseStatus.fromJson(Map<String, dynamic> json):
-    this.status = json[ResponseStatusFields.status],
-    this.message = json[ResponseStatusFields.message],
-    this.code = json[ResponseStatusFields.code],
-    this.data = json[ResponseStatusFields.data],
-    this.dataError = json[ResponseStatusFields.dataError]
-  ;
+  ResponseStatus.fromJson(Map<String, dynamic> json)
+      : this.status = json[ResponseStatusFields.status],
+        this.message = json[ResponseStatusFields.message],
+        this.code = json[ResponseStatusFields.code],
+        this.data = json[ResponseStatusFields.data],
+        this.dataError = json[ResponseStatusFields.dataError];
 
-  ResponseStatus<T3,T4> copyWith<T3,T4>({ 
-    bool status, 
-    String message, 
-    T3 code, 
-    T4 data, 
-  })=> ResponseStatus<T3,T4>(
-    status ?? this.status,
-    message ?? this.message,
-    code: code ?? this.code,
-    data: data
-  );
+  ResponseStatus<T3, T4> copyWith<T3, T4>({
+    bool status,
+    String message,
+    T3 code,
+    T4 data,
+  }) =>
+      ResponseStatus<T3, T4>(status ?? this.status, message ?? this.message,
+          code: code ?? this.code, data: data);
 
   //Status the response success or falid
-  void setStatus(bool status){
-    this.status=status;
+  void setStatus(bool status) {
+    this.status = status;
   }
 
   // Message the response
-  void setMessage(String message){
+  void setMessage(String message) {
     this.message = message;
   }
 
   // Code the response
-  void setCode(T1 code){
+  void setCode(T1 code) {
     this.code = code;
   }
 
   // Data the response
-  void setData(T2 data){
+  void setData(T2 data) {
     this.data = data;
   }
+
   //Error data the response
-  void setDataError(dynamic dataError){
+  void setDataError(dynamic dataError) {
     this.dataError = dataError;
   }
-
 }
-
-
